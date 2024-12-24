cb43460a856dfba03b6bf615a33a84e279afe07c
parliamentbomber <109875196+caernarferon@users.noreply.github.com>
2024-12-24 04:14:08 -0700

    wrong key
    Signed-off-by: parliamentbomber <109875196+caernarferon@users.noreply.github.com>

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAhpXENqenMfsgiJsO5he3Cwhls9f4RYom6S3n5F/w/2"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIARFsmzGSPX4vfQnohmjrwI1QNCSbyZwMzqn4n3wl/FF"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPXZ7ktmDNkJlC71cLnxiZkROE94KPqO2leoK9kp3pLd"
   ];
 in {
   "psk.age".publicKeys = keys;
